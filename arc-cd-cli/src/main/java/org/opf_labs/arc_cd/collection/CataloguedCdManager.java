f84d67b4b22e64c715c3c8271513077b5464426a
carlwilson <carl@openplanetsfoundation.org>
2013-11-04 22:07:03 +0000

    Tidied up JavaDoc comment.

--- arc-cd-cli/src/main/java/org/opf_labs/arc_cd/collection/CataloguedCdManager.java
+++ arc-cd-cli/src/main/java/org/opf_labs/arc_cd/collection/CataloguedCdManager.java
@@ -19,15 +19,13 @@ import org.apache.commons.io.FilenameUtils;
 import com.google.common.base.Preconditions;
 
 /**
- * TODO JavaDoc for CataloguedCdStack.</p>
- * TODO Tests for CataloguedCdStack.</p>
- * TODO Implementation for CataloguedCdStack.</p>
+ * TODO Tests for CataloguedCdStack.
  * 
- * @author  <a href="mailto:carl@openplanetsfoundation.org">Carl Wilson</a>.</p>
- *          <a href="https://github.com/carlwilson">carlwilson AT github</a>.</p>
+ * @author <a href="mailto:carl@openplanetsfoundation.org">Carl Wilson</a>.</p>
+ *         <a href="https://github.com/carlwilson">carlwilson AT github</a>.</p>
  * @version 0.1
  * 
- * Created 1 Oct 2013:16:08:06
+ *          Created 1 Oct 2013:16:08:06
  */
 
 public final class CataloguedCdManager {
@@ -36,44 +34,50 @@ public final class CataloguedCdManager {
 	private final NavigableMap<Integer, CataloguedCd> managedCdRecords;
 	private final Map<Integer, File> catalogueFiles;
 	private Integer currentId = DEFAULT_ID;
-	
+
 	private CataloguedCdManager() {
-		throw new AssertionError("Do not enter default constructor for " + this.getClass().getName());
+		throw new AssertionError("Do not enter default constructor for "
+				+ this.getClass().getName());
 	}
-	
-	private CataloguedCdManager(final Map<Integer, CataloguedCd> cdsToManage, final Map<Integer, File> catalogueFiles) {
+
+	private CataloguedCdManager(final Map<Integer, CataloguedCd> cdsToManage,
+			final Map<Integer, File> catalogueFiles) {
 		this.managedCdRecords = new TreeMap<>(cdsToManage);
 		this.catalogueFiles = new HashMap<>(catalogueFiles);
 		this.managedCdRecords.put(DEFAULT_ID, CataloguedCd.DEFAULT);
 	}
-	
+
 	/**
-	 * @return the current Integer id 
+	 * @return the current Integer id
 	 */
 	public Integer getCurrentId() {
 		return this.currentId;
 	}
-	
+
 	/**
 	 * @return the sorted set of all ids in the catalogue
 	 */
 	public SortedSet<Integer> getAllIds() {
-		return Collections.unmodifiableSortedSet(new TreeSet<>(this.managedCdRecords.keySet()));
+		return Collections.unmodifiableSortedSet(new TreeSet<>(
+				this.managedCdRecords.keySet()));
 	}
-	
+
 	/**
 	 * @param requestedId
 	 * @return the catalogue record for the requested id
 	 */
 	public CataloguedCd getCatalogueRecordById(final Integer requestedId) {
-		if (this.managedCdRecords.containsKey(requestedId)) this.currentId = requestedId;
+		if (this.managedCdRecords.containsKey(requestedId))
+			this.currentId = requestedId;
 		return this.managedCdRecords.get(requestedId);
 	}
-	
+
 	public File getCatalogueFileById(final Integer requestedId) {
-		if (this.catalogueFiles.containsKey(requestedId)) this.currentId = requestedId;
+		if (this.catalogueFiles.containsKey(requestedId))
+			this.currentId = requestedId;
 		return this.catalogueFiles.get(requestedId);
 	}
+
 	/**
 	 * @return the current catalogue record
 	 */
@@ -88,7 +92,7 @@ public final class CataloguedCdManager {
 		this.currentId = this.managedCdRecords.firstKey();
 		return this.managedCdRecords.get(this.currentId);
 	}
-	
+
 	/**
 	 * @return the next catalogue record, this becomes the current record
 	 */
@@ -99,7 +103,7 @@ public final class CataloguedCdManager {
 		}
 		return this.managedCdRecords.get(this.currentId);
 	}
-	
+
 	/**
 	 * @param idToRemove
 	 */
@@ -115,34 +119,40 @@ public final class CataloguedCdManager {
 			}
 		}
 	}
-	
+
 	/**
 	 * @return the number of items in the catalogue
 	 */
 	public int size() {
 		return this.managedCdRecords.size();
 	}
-	
+
 	/**
 	 * Create a new catalogue manager form the info files in a root directory
-	 * @param rootDir the root dir of the collection contain the info files
+	 * 
+	 * @param rootDir
+	 *            the root dir of the collection contain the info files
 	 * @return and new manager instance
 	 * @throws FileNotFoundException
 	 */
-	public static CataloguedCdManager fromDirectory(final File rootDir) throws FileNotFoundException {
+	public static CataloguedCdManager fromDirectory(final File rootDir)
+			throws FileNotFoundException {
 		Preconditions.checkNotNull(rootDir, "rootDir == null");
-		Preconditions.checkArgument(rootDir.isDirectory(), "rootDir.isDirectory != true");
+		Preconditions.checkArgument(rootDir.isDirectory(),
+				"rootDir.isDirectory != true");
 		Map<Integer, CataloguedCd> cataloguedCds = new HashMap<>();
 		Map<Integer, File> catalogueFiles = new HashMap<>();
-		File [] infoFiles = rootDir.listFiles(new FilenameFilter() {
+		File[] infoFiles = rootDir.listFiles(new FilenameFilter() {
 			@Override
 			public boolean accept(final File dir, final String name) {
-				return FilenameUtils.isExtension(name.toLowerCase(), ArchiveCollection.INFO_EXT);
+				return FilenameUtils.isExtension(name.toLowerCase(),
+						ArchiveCollection.INFO_EXT);
 			}
 		});
 		for (File infoFile : infoFiles) {
 			CdItemRecord item = CdItemRecord.fromInfoFile(infoFile);
-			Integer intKey = Integer.valueOf(FilenameUtils.getBaseName(infoFile.getName()));
+			Integer intKey = Integer.valueOf(FilenameUtils.getBaseName(infoFile
+					.getName()));
 			cataloguedCds.put(intKey, CataloguedCd.fromValues(intKey, item));
 			catalogueFiles.put(intKey, infoFile);
 		}

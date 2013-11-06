524b08ef0d8fdd3386873859022703b5423b2405
carlwilson <carl@openplanetsfoundation.org>
2013-11-06 10:10:05 +0000

    Added unimplemented Test class for ArchiveCollection.

---
+++ arc-cd-cli/src/test/java/org/opf_labs/arc_cd/collection/ArchiveCollectionTest.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.opf_labs.arc_cd.collection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author carl
+ *
+ */
+@SuppressWarnings("static-method")
+public class ArchiveCollectionTest {
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#ArchiveCollection(java.lang.String)}.
+	 */
+	@Test
+	public final void testArchiveCollectionString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#ArchiveCollection(java.io.File)}.
+	 */
+	@Test
+	public final void testArchiveCollectionFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#getItemRecord(java.lang.Integer)}.
+	 */
+	@Test
+	public final void testGetItemRecord() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#getItemToc(java.lang.Integer)}.
+	 */
+	@Test
+	public final void testGetItemToc() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#getItemCueSheet(java.lang.Integer)}.
+	 */
+	@Test
+	public final void testGetItemCueSheet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#checkItemManifest(java.lang.Integer)}.
+	 */
+	@Test
+	public final void testCheckItemManifest() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#readItemRecordFromFile(java.io.File)}.
+	 */
+	@Test
+	public final void testReadItemRecordFromFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.opf_labs.arc_cd.collection.ArchiveCollection#getInfoFile(int)}.
+	 */
+	@Test
+	public final void testGetInfoFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
